903de126d88604410bedab6dd8ac5aa1f3565173
Dan Haddock <dhbuild.002@gmail.com>
2020-05-24 18:17:08 +0100

    Link API to UI Searchbox

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,34 @@ import Search from './modules/Search';
 * - Current Receipe Object
 * Shopping List object
 * - Liked Recipes
+
 **/
 const state = {};
 
-const search = new Search('pizza')
-console.log(search);
+const controlSearch = async() => {
+	// 1) Get Query from View
+	const query = 'pizza'
+
+	if (query) {
+		// 2) New Search Object + Add to State
+		state.search = new Search(query);
+
+		// 3)  Clear Previous Search, Show Loading Symbol (Prepare UI for results)
+
+		// 4) Search for Receipes
+		await state.search.getResults();
+
+		// 5) Render results on UI
+		console.log(state.search.result)
+	}
+}
+
+
+document.querySelector('.search').addEventListener('submit', e => {
+	e.preventDefault();
+	controlSearch();
+})
+
+const search = new Search('pizza');
 
 search.getResults();
