006a09306a6fb5803d62da712dfad01a091716a3
Dan Haddock <dhbuild.002@gmail.com>
2020-05-24 13:29:45 +0100

    Refine the data returned to only show the recipes

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 // https://forkify-api.herokuapp.com/api/search
 async function getResults(query) {
-	const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`)
-		console.log(res)
+	try{
+	const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+	const recipes = res.data.recipes;
+		console.log(recipes)
+	}
+	catch (error){
+		alert(error);
+	}
 }
-getResults('Pizza');
+getResults('Pasta');
