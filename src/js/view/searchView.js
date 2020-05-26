210fdff22b90c3e8f9e8271d038c0d9e80168305
Dan Haddock <dhbuild.002@gmail.com>
2020-05-26 17:40:01 +0100

    Add in Char limitation to recip results

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -3,13 +3,24 @@ import { elements } from './base';
 export const getInput = () => elements.searchInput.value;
 
 export  const clearInput = () => {
-
 	elements.searchInput.value = ' ';
-
 }
-
 export const clearResults = () => {
 	elements.searchList.innerHTML = ' ';
+}
+
+const abbrTitle = (title, limit = 17) => {
+	const newTitle = [];
+	if (title.length > limit) {
+		title.split(' ').reduce((acc, cur) => {
+			if (acc + cur.length <= limit) {
+				newTitle.push(cur);
+			}
+			return acc + cur.length;
+		}, 0);
+		return `${newTitle.join(' ')} ...`;
+	}
+	return title;
 }
 const renderRecipe = recipe => {
 	const markup =
@@ -19,14 +30,14 @@ const renderRecipe = recipe => {
 							<img src="${recipe.image_url}" alt="Test">
 					</figure>
 					<div class="results__data">
-							<h4 class="results__name">${recipe.title}.</h4>
+							<h4 class="results__name">${abbrTitle(recipe.title)}.</h4>
 							<p class="results__author">${recipe.publisher}</p>
 					</div>
 			</a>
 	</li>`;
 	elements.searchList.insertAdjacentHTML('beforeend', markup);
-}
+};
 export const renderResults = recipes => {
 	console.log(recipes);
 	recipes.forEach(renderRecipe);
-}
+};
