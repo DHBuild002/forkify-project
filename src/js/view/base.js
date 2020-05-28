940e3b3eb0a1d187f1f6022de5c1332675d4e7fc
Dan Haddock <dhbuild.002@gmail.com>
2020-05-28 20:57:39 +0100

    Navigation buttons - WIP

--- src/js/view/base.js
+++ src/js/view/base.js
@@ -3,6 +3,7 @@ export const elements = {
 		searchInput: document.querySelector('.search__field'),
 		searchList: document.querySelector('.results__list'),
 		searchRes: document.querySelector('.results'),
+		searchResPages: document.querySelector('.results__pages')
 };
 
 export const elementStrings = {
--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -38,25 +38,42 @@ const renderRecipe = recipe => {
 	elements.searchList.insertAdjacentHTML('beforeend', markup);
 };
 
-const createButton = (page, type)
-const renderButtons = (page, numResults, resultsPerPage) => {
-	const pages = numResults / resPerPage;
+// type: Prev / Next
+const createButton = (page, type) =>
+`<!--
+<button class="btn-inline results__btn--${type}" data-goto= ${type === 'prev' ? page - 1 : page + 1}>
+		<svg class="search__icon">
+				<use href="img/icons.svg#icon-triangle- ${type === 'prev' ? 'left' : 'right'}"></use>
+		</svg>
+		<span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+</button>
+-->
+`;
+const renderButtons = (page, numResults, resPerPage) => {
+	const pages = Math.ceil(numResults / resPerPage);
 
+	let button;
 	if (page === 1 && pages > 1) {
 		// button to go to next page
-
+		button = createButton(page, 'next')
 
 	}else if (page < pages){
 		// we want both next and previous buttons
-
+		button = `
+						  ${createButton(page, 'next')}
+						  ${createButton(page, 'prev')}
+							`;
 	} else if (page === pages && pages > 1) {
 		// show only last button but one
+		button = createButton(page, 'prev');
 	}
+		elements.searchResPages.insertAdjacentHTML('afterbegin', button);
 };
-export const renderResults = (recipes, page = 1 , resPerPage = 3) => {
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
 	const start = (page - 1) * resPerPage;
 	const end = page * resPerPage;
 
 	recipes.slice(start, end).forEach(renderRecipe);
-
+	renderButtons(page, recipes.length, resPerPage);
 };
